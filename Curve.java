dcaff174a6ea39ac7b9a726749a13f1ebfa8a8da
Micah Cowell <micahcowell@gmail.com>
2016-09-03 10:22:31 -0700

    style update

--- Curve.java
+++ Curve.java
@@ -1,24 +1,25 @@
 import java.util.ArrayList;
-/**
+
+/*
  * Coders in the Community
  * 
  * Dan Avila
  * Making the world a better place one line at a time.
  */
-public class Curve
-{
 
-   ArrayList<Double> scores= new ArrayList<Double>();   
-   public double getPoints()
-   {
-   double average = 80;
-   double sum = 0;
-   for(int k = 0; k< scores.size(); k++)
-   {
-       sum += scores.get(k);
-   }
-   return sum/scores.size()-average;
-}
+public class Curve 
+{
+	ArrayList<Double> scores= new ArrayList<Double>();   
+	public double getPoints() 
+	{
+		double average = 80;
+		double sum = 0;
+		for(int k = 0; k< scores.size(); k++) 
+		{
+			sum += scores.get(k);
+		}
+		return sum/scores.size()-average;
+	}
 }
 
 
